25e31ea673f974086408c962c163b1c0dab2e200
Michael <m.cmsrus@gmail.com>
2022-11-14 17:39:08 +0100

    cat class add Barsik, new cat. add function add getter and setter

--- src/Cat.java
+++ src/Cat.java
@@ -4,6 +4,38 @@ public class Cat {
     int age;
     String color;
 
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public Cat() {
     }
 
